bfdade272f86cb4e06b956ee51312f26a8ab9458
Ed Statham <edward.statham@gmail.com>
2025-08-09 12:52:55 +1200

    initial commit

---
+++ src/main/java/com/example/galaxy_sim/GalaxySimApplication.java
@@ -0,0 +1,13 @@
+package com.example.galaxy_sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxySimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxySimApplication.class, args);
+	}
+
+}
